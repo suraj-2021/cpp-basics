ddfed567cfb75af1b0bbb3ff0f66d31d7aa0e53d
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2025-01-29 16:29:11 +0530

    Create basicConstructor.cpp
    Here we conveyed the use of the constructor in cpp in very simplistic way.

---
+++ basicConstructor.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+class Student
+{
+    public:
+    Student(){
+        cout<<"Constructer initilized!"<<endl;
+    }
+};
+
+
+int main(){
+    
+    Student x,y,z;
+    
+}
